b993a679bb227eafad83ccaf665b0839a9d982b2
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-11-03 16:24:53 -0300

    database configured

---
+++ backend/src/main/java/com/zanotti/springreact/entities/Seller.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zanotti.springreact.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author leonardozanotti
+ */
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<>();
+    
+    public Seller() {}
+    
+    public Seller(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public List<Sale> getSales() {
+        return this.sales;
+    }
+}
