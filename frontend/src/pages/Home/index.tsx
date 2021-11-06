d4fb9ba0b791924162b09824ae7aadcd8e7bbe56
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-11-06 09:32:23 -0300

    home and dashboard navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,28 @@
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Spring react</h1>
+          <p className="lead">Analyse the performance of your sales with different perspectives</p>
+          <hr />
+          <p>
+            This application consists in a dashboard, that receives data from the spring boot backend, and then makes
+            graphs and tables to show the data in a beutier way
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
